d103b5e7a65f9a62f658544c7ee969f2e36924bd
Joveth-Dev <jovethespiritu.developer@gmail.com>
2024-01-09 18:45:36 +0800

    Created: store admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Store
 
-# Register your models here.
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "name"]
+    search_fields = ["user__username", "name"]
