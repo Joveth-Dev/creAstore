c7930c1964772149b79cf93d3722ff42f0f6ad49
Joveth-Dev <jovethespiritu.developer@gmail.com>
2024-01-10 12:49:00 +0800

    Added: necessarry store model fields

--- store_app/admin.py
+++ store_app/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store, StoreType
+
+
+@admin.register(StoreType)
+class StoreTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
 
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "name"]
+    list_display = [
+        "id",
+        "name",
+        "user",
+        "type",
+        "address",
+        "contact_number",
+        "open_from",
+        "open_to",
+        "created_at",
+    ]
     search_fields = ["user__username", "name"]
+    list_per_page = 20
+    list_select_related = ["user", "type"]
--- store_app/models.py
+++ store_app/models.py
@@ -1,16 +1,46 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 
 User = get_user_model()
 
 
+class StoreType(models.Model):
+    name = models.CharField(_("Store Type"), max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Store(models.Model):
     user = models.ForeignKey(
         User, verbose_name=_("Store Owner"), on_delete=models.CASCADE
     )
+    type = models.ForeignKey(
+        StoreType, verbose_name=_("Store Type"), on_delete=models.CASCADE
+    )
     name = models.CharField(_("Store Name"), max_length=50, unique=True)
+    description = models.TextField()
+    address = models.CharField(
+        _("Store Address"), max_length=255, blank=True, null=True
+    )
+    contact_number = models.CharField(
+        max_length=50,
+        validators=[
+            RegexValidator(
+                regex=r"^\+?63(?:\d{9}|\d{10})$",
+                message="Enter a valid Philippine phone number.",
+            )
+        ],
+        blank=True,
+        null=True,
+    )
+    open_from = models.TimeField(blank=True, null=True)
+    open_to = models.TimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
